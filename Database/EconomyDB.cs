5ed1ee58f64426aa251469cf7784700439d0937a
Feli <64453816+01-Feli@users.noreply.github.com>
2020-10-13 17:37:29 -0300

    Update EconomyDB.cs

--- Database/EconomyDB.cs
+++ Database/EconomyDB.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
